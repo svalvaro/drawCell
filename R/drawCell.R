9b9016e9696064316f158fb411e4b22881d9ceba
svalvaro <sanchezvillalba.alvaro@gmail.com>
2022-10-31 19:14:57 +0100

    feat: Removed old functions that were based on webshot2, added functionality based on vector images and htmlWidgets.

--- R/drawCell.R
+++ R/drawCell.R
@@ -11,9 +11,7 @@
 #'
 #' @param color Highlited color of the subcelullar components.
 #'
-#' @import rmarkdown
-#' @importFrom  webshot2 webshot
-#' @importFrom taxize get_ids
+#' @import htmlwidgets
 #'
 #' @return Generates a PNG of the requested cell by the user with highligthed
 #' subcellular elements.
@@ -24,111 +22,26 @@
 #' sl_ids = '0049',
 #' size = 2000,
 #' color = 'lightblue')
-drawCell <- function(organism_identifier,
-                    sl_ids,
-                    color = 'blue',
-                    size = 2000,
-                    delay = 1,
-                    returnPlot = TRUE
-                    ){
+#' <Add Title>
+#'
+#' <Add Description>
+drawCell <- function(organism_identifier, sl_ids, inputId = NULL, width = NULL, height = NULL, elementId = NULL, colour_sl = NULL) {
 
-    # Organism can be provided the taxonomy id or the name of the species.
-
-    #IF the user provides a number ( the taxonomy id itself)
-
-    # If is a species 'Homo sapiens' when as.numeric it will return NA
-
-    if (is.na(as.numeric(organism_identifier))) {
-
-        taxonomy_id <- taxize::get_ids(
-            sci_com = organism_identifier,
-            db = 'ncbi'
-        )
-
-        taxonomy_id <- taxonomy_id$ncbi[1]
-    }else{
-        taxonomy_id <- organism_identifier
-    }
-
-
-
-    # Generate a cell_pictures folder if it does not exist
-
-    # dir_cell_pic <- system.file('cell_pictures/', package = 'drawCell')
-    #
-    # if (base::dir.exists(dir_cell_pic) == FALSE) {
-    #     base::dir.create(paste0(system.file(package = 'drawCell'),
-    #                             '/','cell_pictures'))
-    # }
-
-
-    #First generate custom_element
-
-    drawCell:::generate_custom_element()
-
-    #Determine the template
-    input = system.file("Rmd_template/html_generator.Rmd", package="drawCell")
-
-    #output_dir <-  system.file("cell_pictures/", package="drawCell")
-
-    htmlOutTmp <- base::tempdir()
-
-    name_picture <- 'cell_picture'
-
-    # Edit the JS code with the color of the user:
-
-    drawCell:::edit_jscode(color = color)
-
-    # Create hTML picture
-
-    rmarkdown::render(
-        input = input,
-        params = list(taxonomy_id = taxonomy_id,
-                    sl_ids = sl_ids,
-                    delay = delay),
-        output_file = name_picture,
-        output_dir = htmlOutTmp
-    )
-
-    # Edit the HTML with the specified size:
-
-    drawCell:::edit_html(size = size,
-                         html_path = paste0(htmlOutTmp,'/',name_picture, '.html') )
-
-    # Restore the colour to the original js code
-
-    drawCell:::restore_jscode(color = color)
-
-    # Create webshot as a temp file
-
-    destFile <- base::tempfile(fileext = '.png')
-
-    message(paste0('The tmp file is: ', destFile))
-
-    # Create a png out of the html output with webshot2.
-    # It requires to have google chrome installed.
-
-    webshot2::webshot(url = paste0(htmlOutTmp,'/',name_picture, '.html'),
-                    file = destFile,
-                    vwidth = 2000,
-                    vheight = 2000,
-                    delay = delay)
-
-
-
-    if (returnPlot == TRUE) {
-        # Lastly render the picture as a plot
-
-        cellPNG <- png::readPNG(source = destFile)
-
-        message('Cell picture created: ', !is.null(cellPNG))
-
-        return(
-            grid::grid.raster(cellPNG)
-        )
-    }else{
-        return(destFile)
-    }
-
-
+  
+  # forward options using x
+  x = list(
+    input_id = inputId,
+    organism_identifier = organism_identifier,
+    sl_ids = csl(sl_ids),
+    colour_sl = csl(colour_sl)
+  )
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'drawCell',
+    x,
+    width = width,
+    height = height,
+    package = 'drawCell',
+    elementId = elementId
+  )
 }
