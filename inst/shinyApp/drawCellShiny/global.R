9c9c4221dee09d8ccf52610dc90ddc078a596075
Recle E. Vibal <recle.vibal@appsilon.com>
2022-11-03 14:35:17 +0800

    Chore: Removes unnecessary code

--- inst/shinyApp/drawCellShiny/global.R
+++ inst/shinyApp/drawCellShiny/global.R
@@ -7,14 +7,3 @@ data = read.csv2(path, sep = ";")
 path = system.file("extdata", "uniprot.tsv", package = "drawCell")
 uniprot = read.table(file = path, sep = '\t', header = TRUE)
 uniport_sc_ids = substr(uniprot$Subcellular.location.ID, 4, 7)
-
-cell_name <- unique(data$Image.name)
-cell_name <- cell_name[!is.na(cell_name)]
-
-csl <- function(items) {
-  return(paste(paste(htmltools::htmlEscape(unlist(items)), collapse = ",", sep = ""), sep = ""))
-}
-
-find_sc = function(id) {
-  uniprot[which(uniport_sc_ids == id), ]$Name
-}
