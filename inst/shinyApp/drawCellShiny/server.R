9fcff1b077164762df5cd2ef21bc50817d16152d
Recle E. Vibal <recle.vibal@appsilon.com>
2022-11-03 11:21:27 +0800

    Feat: Uses original apps IDs
    I think it is much easier to track changes with this.
    
    I was able to make the table to work, but still needs more
    styling.
    
    Button added to clear the colors and selected subcellular
    locations, but not yet working.

--- inst/shinyApp/drawCellShiny/server.R
+++ inst/shinyApp/drawCellShiny/server.R
@@ -1,80 +1,89 @@
 function(input, output) {
   taxonomy_id <- reactive({
-    if (input$selected_cell == "") {
-      if (is.na(as.numeric(input$cell_taxonomy_id))) {
+    if (input$cell_type == "") {
+      if (is.na(as.numeric(input$taxIdInput))) {
         taxonomy_id <- taxize::get_ids(
-          sci_com = input$cell_taxonomy_id,
+          sci_com = input$taxIdInput,
           db = "ncbi"
         )
 
         taxonomy_id <- as.numeric(taxonomy_id$ncbi[1])
       } else {
-        taxonomy_id <- as.numeric(input$cell_taxonomy_id)
+        taxonomy_id <- as.numeric(input$taxIdInput)
       }
     } else {
-      taxonomy_id <- data[which(data$Image.name == input$selected_cell), ]$TaxID
+      taxonomy_id <- data[which(data$Image.name == input$cell_type), ]$TaxID
     }
     return(taxonomy_id)
   })
 
   sc_id <- reactiveVal()
-  sc_id_to_select <- reactiveVal()
-  colours_vector <- reactiveVal("#56B4E9")
 
-  output$cell_output <- renderDrawCell({
+  subcelular_colours <- reactiveVal(list("SL0000" = "#56B4E9"))
+
+  output$cell_output <- drawCell:::renderDrawCell({
     drawCell(
       organism_identifier = taxonomy_id(),
-      sl_ids = sc_id_to_select(),
-      inputId = "cell",
-      colour_sl = colours_vector()
+      list_sl_colors = subcelular_colours()
     )
   })
 
   observeEvent(input$cell_click, {
     sc_id(substr(input$cell_click, 3, 6))
-    sc_id_to_select(c(sc_id_to_select(), input$cell_click))
-    colours_vector(c(colours_vector(), input$selected_sub_cell_color))
+
+    list_named_colours <- c(subcelular_colours(), input$colourInput)
+    names(list_named_colours)[length(list_named_colours)] <- input$cell_click
+    subcelular_colours(list_named_colours)
   })
 
-  observeEvent(input$selected_cell, {
+  observeEvent(input$colourInput, {
+    list_named_colours <- subcelular_colours()
+    list_named_colours[length(list_named_colours)] <- input$colourInput
+    subcelular_colours(list_named_colours)
+  })
+
+  observeEvent(input$cell_type, {
     sc_id(NULL)
-    sc_id_to_select("SL0000")
   })
 
   output$selected_cell_name <- renderText({
-    input$selected_cell
+    input$cell_type
   })
 
   output$cell_sl_color <-
     DT::renderDataTable({
-      req(sc_id_to_select() > 1)
+      req(length(subcelular_colours()) > 1)
 
-      if (length(sc_id_to_select()) == 1 && sc_id_to_select() == "SL0000") {
+      if (length(subcelular_colours()) == 1 && names(subcelular_colours()) == "SL0000") {
         selected_sc <- data.frame()
       } else {
         selected_sc <- uniprot[
-          uniprot$Subcellular.location.ID %in% gsub("SL", "SL-", sc_id_to_select()),
+          uniprot$Subcellular.location.ID %in% gsub("SL", "SL-", names(subcelular_colours())),
           c("Subcellular.location.ID", "Name")
         ]
 
         sc_colors <-
-          # By default, SL0000 will have the first default color, so this must be removed
-          # since SL0000 does not need to and will not be displayed in the table anyway.
           glue::glue(
-            "<i class='square icon' style='visibility: visible; color: {colours_vector()[-1]}'></i>"
+            "<div class='ui label'
+            style='visibility: visible;
+            color: white;
+            background-color: {subcelular_colours()}'>
+            {subcelular_colours()}
+            </div>"
           )
 
-        names(sc_colors) <- gsub("SL", "SL-", sc_id_to_select())[-1]
+        names(sc_colors) <- gsub("SL", "SL-", names(subcelular_colours()))
 
-        selected_sc$Colors <- sc_colors[selected_sc$Subcellular.location.ID]
+        selected_sc$Color <- sc_colors[selected_sc$Subcellular.location.ID]
       }
 
       semantic_DT(
-        selected_sc,
-        colnames = c("Subcellular Location ID", "Subcellular Name", "Color"),
+        selected_sc[, c("Name", "Color")],
+        colnames = c("Subcellular Name", "Color"),
         escape = FALSE,
         options = list(
           searching = FALSE,
+          scrollX = TRUE,
           paging = FALSE,
           info = FALSE,
           columnDefs = list(
