494f8bb1f611542f76d5e6358bc4cd37f7fae7e0
Recle E. Vibal <recle.vibal@appsilon.com>
2022-11-04 21:31:13 +0800

    Chore: Removes explicit imports in NAMESPACE
    This will produce a note in R CMD CHECK,
    but it should pass.

--- R/drawCellShiny.R
+++ R/drawCellShiny.R
@@ -1,13 +1,6 @@
 #' Run Shiny app for the drawCell Package
 #'
 #' @export
-#'
-#' @import shiny
-#' @import shiny.semantic
-#' @import htmltools
-#' @import glue
-#' @import colourpicker
-#' @import DT
 drawCellShiny <- function() {
   shiny::shinyAppDir(appDir = base::system.file("shinyApp/drawCellShiny",
     package = "drawCell"
