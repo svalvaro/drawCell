957b1787578df75af0312e048088d3495e586d8f
Recle E. Vibal <recle.vibal@appsilon.com>
2022-11-09 16:52:06 +0800

    Feat: Increase cell size to 150%
    This is the only shiny solution that works so far.

--- inst/shinyApp/drawCellShiny/ui.R
+++ inst/shinyApp/drawCellShiny/ui.R
@@ -88,7 +88,7 @@ semanticPage(
         ),
         segment(
           class = "padded no_box",
-          drawCell:::drawCellOutput("cell_output", height = "100%", width = "100%")
+          drawCell:::drawCellOutput("cell_output", height = "150%", width = "150%")
         )
       ),
       div(
